e77d4f0de2ea4189a249d32627c4fbef425cc71a
iamgame3 <kosmosworld9@hotmail.com>
2022-12-04 14:10:57 -0500

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ let multiply = (a, b) => {
 let divide = (a, b) => {
     return a / b;
 };
+
+let operate = (func, a, b) => {
+    if (func = 'add') return add(a, b);
+    if (func = 'subtract') return subtract(a, b);
+    if (func = 'multiply') return multiply(a, b);
+    if (func = 'divide') return divide(a, b);
+};
