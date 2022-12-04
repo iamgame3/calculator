351afc3c846aa092e91f717e7ecb1822d73b727f
iamgame3 <kosmosworld9@hotmail.com>
2022-12-04 16:16:57 -0500

    Add eventlistener to change display when numbers are pressed

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let subtract = (a, b) => {
     return a - b;
 };
 
+const display = document.querySelector('.display');
+const digits = document.querySelectorAll('.digit'); 
+let displayValue = 0;
+
 let multiply = (a, b) => {
     return a * b;
 };
@@ -20,3 +24,10 @@ let operate = (func, a, b) => {
     if (func == 'multiply') return multiply(a, b);
     if (func == 'divide') return divide(a, b);
 };
+
+digits.forEach(digit => digit.addEventListener('click', () => {
+    console.log(digit.textContent)
+    if (display.textContent == 0) display.textContent = '';
+    display.textContent += digit.textContent;
+    displayValue += digit.textContent;
+}));
