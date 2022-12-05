df2b5ade3e9f88cb8d05ade8dfe907744352b10f
iamgame3 <kosmosworld9@hotmail.com>
2022-12-04 19:45:50 -0500

    Make chained operations work

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operation');
 const clear = document.querySelector('.clear');
 const calculate = document.querySelector('#equals')
+let result = 0;
 let Value1 = 0;
-let Value2 = 0;
+let Value2 = '';
 let func = 0;
 
 let add = (a, b) => {
@@ -24,6 +25,7 @@ let divide = (a, b) => {
 };
 
 let operate = (func, a, b) => {
+    console.log(func, a, b);
     if (func == '+') return add(a, b);
     if (func == '-') return subtract(a, b);
     if (func == 'x') return multiply(a, b);
@@ -32,10 +34,10 @@ let operate = (func, a, b) => {
 
 digits.forEach(digit => digit.addEventListener('click', () => {
     if (display.textContent == 0) display.textContent = '';
-    if (func && Value2 == 0) display.textContent = '';
+    if (func && Value2 == '') display.textContent = '';
     if (func) {
-        display.textContent += digit.textContent;
-        Value2 += digit.textContent;
+        display.textContent = digit.textContent;
+        Value2 = digit.textContent;
         return;
     };
     display.textContent += digit.textContent;
@@ -43,7 +45,16 @@ digits.forEach(digit => digit.addEventListener('click', () => {
 }));
 
 operators.forEach(operator => operator.addEventListener('click', () => {
-    console.log(operator.textContent);
+    if (func == operator.textContent && Value1 && Value2) {
+        display.textContent = operate(func, Value1, Value2);
+        Value1 = operate(func, Value1, Value2);
+        return;
+    };
+    if (func && Value1 && Value2) {
+        display.textContent = operate(func, Value1, Value2);
+        Value1 = operate(func, Value1, Value2);
+        return func = operator.textContent;
+    };
     func = operator.textContent;
 }));
 
@@ -51,10 +62,11 @@ clear.addEventListener('click', () => {
     Value1 = 0;
     Value2 = 0;
     func = 0;
+    result = 0;
     display.textContent = 0;
 });
 
 calculate.addEventListener('click', () => {
     display.textContent = operate(func, Value1, Value2);
     Value1 = operate(func, Value1, Value2);
-})
+});
