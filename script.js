273b5d9337bc31ea923cf6048c48acf6e930e8bc
iamgame3 <kosmosworld9@hotmail.com>
2022-12-06 20:22:00 -0500

    Allow multiple digits to be added to second value when calculating

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let result = 0;
 let Value1 = 0;
 let Value2 = '';
 let func = 0;
+let Value2New = 0;
 
 let add = (a, b) => {
     return parseInt(a) + parseInt(b);
@@ -26,33 +27,46 @@ let divide = (a, b) => {
 
 let operate = (func, a, b) => {
     console.log(func, a, b);
-    if (func == '+') return add(a, b);
-    if (func == '-') return subtract(a, b);
-    if (func == 'x') return multiply(a, b);
-    if (func == '÷') return divide(a, b);
+    if (func == '+') result = add(a, b);
+    if (func == '-') result = subtract(a, b);
+    if (func == 'x') result = multiply(a, b);
+    if (func == '÷') result = divide(a, b);
+    Value2New = 0;
+    return [result, Value2New];
 };
 
 digits.forEach(digit => digit.addEventListener('click', () => {
+    console.log(Value1);
+    console.log(Value2);
     if (display.textContent == 0) display.textContent = '';
     if (func && Value2 == '') display.textContent = '';
-    if (func) {
+    if (func && !Value2New) {
         display.textContent = digit.textContent;
         Value2 = digit.textContent;
+        console.log("Hi");
+        Value2New = 1;
+        return;
+    }
+    if (func) {
+        display.textContent += digit.textContent;
+        Value2 += digit.textContent;
+        console.log("Hello");
         return;
     };
+    console.log("Greetings");
     display.textContent += digit.textContent;
     Value1 += digit.textContent;
 }));
 
 operators.forEach(operator => operator.addEventListener('click', () => {
     if (func == operator.textContent && Value1 && Value2) {
-        display.textContent = operate(func, Value1, Value2);
-        Value1 = operate(func, Value1, Value2);
+        display.textContent = (operate(func, Value1, Value2))[0];
+        Value1 = (operate(func, Value1, Value2))[0];
         return;
     };
     if (func && Value1 && Value2) {
-        display.textContent = operate(func, Value1, Value2);
-        Value1 = operate(func, Value1, Value2);
+        display.textContent = (operate(func, Value1, Value2))[0];
+        Value1 = (operate(func, Value1, Value2))[0];
         return func = operator.textContent;
     };
     func = operator.textContent;
@@ -63,10 +77,11 @@ clear.addEventListener('click', () => {
     Value2 = 0;
     func = 0;
     result = 0;
+    Value2New = 0;
     display.textContent = 0;
 });
 
 calculate.addEventListener('click', () => {
-    display.textContent = operate(func, Value1, Value2);
-    Value1 = operate(func, Value1, Value2);
+    display.textContent = (operate(func, Value1, Value2))[0];
+    Value1 = (operate(func, Value1, Value2))[0];
 });
