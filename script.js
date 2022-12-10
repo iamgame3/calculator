8279e4843eec32a3f1ec9f315074453945e76ed1
iamgame3 <kosmosworld9@hotmail.com>
2022-12-09 22:29:18 -0500

    Change all click events to functions, add keyboard support and remove console.logs

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ let divide = (a, b) => {
 };
 
 let operate = (func, a, b) => {
-    console.log(func, a, b);
     if (Value1 == "ERROR" || Value2 == "ERROR" || display.textContent == "ERROR") return ["ERROR", clearData(0)];
     if (!func) {
         if (Value1 == '') {
@@ -44,7 +43,6 @@ let operate = (func, a, b) => {
     if (func == '÷') result = divide(a, b);
     if (!result && result !== 0) return ["ERROR", clearData(0)];
     if (result > 999999999) return ["ERROR", clearData(0)];
-    console.log('boom');
     if (result < 0.000001 && result !== 0) {
         if ((result + 0.000001) < 0) {
             Value2New = 0;
@@ -59,16 +57,90 @@ let operate = (func, a, b) => {
         return ["ERROR", Value1, clearData(0)];
         };
     };
-    console.log('boom');
     if ((result.toString()).length > 9) {
         tempResult = result.toString()
         tempResultFixed = tempResult.slice(0, 9);
         result = parseFloat(tempResultFixed);
     };
-    console.log(result.length);
     Value2New = 0;
     return [result, Value2New];
 };
+
+let digitPress = (a) => {
+    if (Value1 == "ERROR" || Value2 == "ERROR" || display.textContent == "ERROR") return ["ERROR", clearData(0)];
+    if (a.textContent) a = a.textContent;
+    if (display.textContent === '0') display.textContent = '';
+    if (func && Value2 == '') display.textContent = '';
+    if (func && !Value2New) {
+        display.textContent = a;
+        Value2 = a;
+        Value2New = 1;
+        return;
+    }
+    if (display.textContent == 0 && !Value1) Value1 = '';
+    if (func) {
+        if (a == '.' && (display.textContent === '0' || display.textContent == '' || Value2 == '')) {
+            display.textContent = '0.';
+        }   else {
+        display.textContent += a;
+        };
+        Value2 += a;
+        if (display.textContent.length > 9) {
+            tempResultFixed = display.textContent.slice(0, 9);
+            display.textContent = tempResultFixed;
+            Value2 = display.textContent;
+        };
+        decimalCheck = display.textContent.split('.').length - 1;
+        if (decimalCheck > 1) {
+            tempResultFixed = display.textContent.slice(0, -1);
+            display.textContent = tempResultFixed;
+            Value2 = display.textContent;
+        };
+        return;
+    };
+    if (a == 0 && (display.textContent === '0' || display.textContent == '')) {
+        display.textContent = a;
+        Value1 = '';
+        return;
+    };
+    if (a == '.' && (display.textContent === '0' || display.textContent == '')) {
+        display.textContent = '0.';
+    }   else {
+    display.textContent += a;
+    };
+    Value1 += a;
+    if (display.textContent.length > 9) {
+        tempResultFixed = display.textContent.slice(0, 9);
+        display.textContent = tempResultFixed;
+        Value1 = display.textContent;
+    };
+    decimalCheck = display.textContent.split('.').length - 1;
+    if (decimalCheck > 1) {
+        tempResultFixed = display.textContent.slice(0, -1);
+        display.textContent = tempResultFixed;
+        Value2 = display.textContent;
+    };
+};
+
+let operatorPress = (a) => {
+    if (a.textContent) a = a.textContent;
+    if (a == '*') a = 'x';
+    if (a == '/') a = '÷';
+    if (func == a && Value1 && Value2) {
+        display.textContent = (operate(func, Value1, Value2))[0];
+        Value1 = (operate(func, Value1, Value2))[0];
+        return;
+    };
+    if (func && Value1 && Value2 && Value2New == 0) {
+        return func = a;
+    };
+    if (func && Value1 && Value2) {
+        display.textContent = (operate(func, Value1, Value2))[0];
+        Value1 = (operate(func, Value1, Value2))[0];
+        return func = a;
+    };
+    func = a;
+}
 
 let clearData = (a) => {
     if (!a) {
@@ -83,95 +155,12 @@ let clearData = (a) => {
     Value2New = 0;
 };
 
-digits.forEach(digit => digit.addEventListener('click', () => {
-    console.log(Value1);
-    console.log(Value2);
-    if (Value1 == "ERROR" || Value2 == "ERROR" || display.textContent == "ERROR") return ["ERROR", clearData(0)];
-    if (display.textContent === '0') display.textContent = '';
-    if (func && Value2 == '') display.textContent = '';
-    if (func && !Value2New) {
-        display.textContent = digit.textContent;
-        Value2 = digit.textContent;
-        console.log("Hi");
-        Value2New = 1;
-        return;
-    }
-    if (display.textContent == 0 && !Value1) Value1 = '';
-    if (func) {
-        if (digit.textContent == '.' && (display.textContent === '0' || display.textContent == '' || Value2 == '')) {
-            display.textContent = '0.';
-        }   else {
-        display.textContent += digit.textContent;
-        };
-        Value2 += digit.textContent;
-        if (display.textContent.length > 9) {
-            tempResultFixed = display.textContent.slice(0, 9);
-            display.textContent = tempResultFixed;
-            Value2 = display.textContent;
-        };
-        decimalCheck = display.textContent.split('.').length - 1;
-        if (decimalCheck > 1) {
-            tempResultFixed = display.textContent.slice(0, -1);
-            display.textContent = tempResultFixed;
-            Value2 = display.textContent;
-        };
-        console.log("Hello");
-        return;
-    };
-    console.log("Greetings");
-    if (digit.textContent == 0 && (display.textContent === '0' || display.textContent == '')) {
-        display.textContent = digit.textContent;
-        Value1 = '';
-        return;
-    };
-    console.log('Good Day')
-    if (digit.textContent == '.' && (display.textContent === '0' || display.textContent == '')) {
-        display.textContent = '0.';
-    }   else {
-    display.textContent += digit.textContent;
-    };
-    Value1 += digit.textContent;
-    if (display.textContent.length > 9) {
-        tempResultFixed = display.textContent.slice(0, 9);
-        display.textContent = tempResultFixed;
-        Value1 = display.textContent;
-    };
-    decimalCheck = display.textContent.split('.').length - 1;
-    console.log(decimalCheck);
-    if (decimalCheck > 1) {
-        tempResultFixed = display.textContent.slice(0, -1);
-        display.textContent = tempResultFixed;
-        Value2 = display.textContent;
-    };
-}));
-
-operators.forEach(operator => operator.addEventListener('click', () => {
-    if (func == operator.textContent && Value1 && Value2) {
-        display.textContent = (operate(func, Value1, Value2))[0];
-        Value1 = (operate(func, Value1, Value2))[0];
-        return;
-    };
-    if (func && Value1 && Value2 && Value2New == 0) {
-        return func = operator.textContent;
-    };
-    if (func && Value1 && Value2) {
-        display.textContent = (operate(func, Value1, Value2))[0];
-        Value1 = (operate(func, Value1, Value2))[0];
-        return func = operator.textContent;
-    };
-    func = operator.textContent;
-}));
-
-clear.addEventListener('click', () => {
-    clearData(1);
-});
-
-calculate.addEventListener('click', () => {
+let calculation = () => {
     display.textContent = (operate(func, Value1, Value2))[0];
     Value1 = (operate(func, Value1, Value2))[0];
-});
+};
 
-del.addEventListener('click', () => {
+let deletion = () => {
     if (display.textContent == "ERROR") return;
     if (Value2New = 1 && Value2) {
         display.textContent = Value2.slice(0, -1);
@@ -184,9 +173,9 @@ del.addEventListener('click', () => {
         if (display.textContent == '') display.textContent = 0;
         return;
     };
-});
+}
 
-sign.addEventListener('click', () => {
+let signChange = () => {
     if (display.textContent == "ERROR") return;
     tempValue1 = '0' + Value1;
     tempValue2 = '0' + Value2;
@@ -197,4 +186,35 @@ sign.addEventListener('click', () => {
         Value2 *= -1;
         display.textContent = Value2;
     };
+};
+
+digits.forEach(digit => digit.addEventListener('click', () => {
+    digitPress(digit);
+}));
+
+operators.forEach(operator => operator.addEventListener('click', () => {
+    operatorPress(operator);
+}));
+
+clear.addEventListener('click', () => {
+    clearData(1);
+});
+
+calculate.addEventListener('click', calculation);
+
+del.addEventListener('click', deletion);
+
+sign.addEventListener('click', signChange);
+
+document.addEventListener('keydown', (event) => {
+    key = event.key;
+    if (key == 'End') clearData(1);
+    if (display.textContent == "ERROR") return;
+    if (key == ' ') return;
+    if (key == 'Enter' || key == ' ') event.preventDefault();
+    if (isNaN(key) == false || key == '.') digitPress(key);
+    if (key == '+' || key == '-' || key == '*' || key == '/') operatorPress(key);
+    if (key == 'Enter' || key == '=') calculation();
+    if (key == 'Backspace' || key == 'Delete') deletion();
+    if (key == 'Insert') signChange();
 });
